9df4fb19074d1117c1b384ba1fc0eef498c02a29
Naoki Sano <grand4352@hotmail.co.jp>
2017-02-23 15:10:40 +0900

    タイトルロゴとPushStartの配置とタイトルスクリプト追加

---
+++ FlyHigh_Speeder/Assets/Scripts/Titles/TitleMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
